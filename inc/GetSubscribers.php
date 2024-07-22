99111cd9c954605a030bf072be18a638132057a4
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2024-07-22 14:21:01 +0300

    Add files via upload

--- inc/GetSubscribers.php
+++ inc/GetSubscribers.php
@@ -29,15 +29,15 @@ class suno_GetSubscribers{
 		//	'email'=> sanitize_text_field($_GET['email']),
 		//);
 		$temp = [];
-		if (isset($_GET['email']) and !wp_verify_nonce($_POST['email'],'name_of_my_action')) $temp['email'] = sanitize_text_field($_GET['email']);
-		if (isset($_GET['subject']) and !wp_verify_nonce($_POST['subject'],'name_of_my_action')) $temp['subject'] = sanitize_text_field($_GET['subject']);
-		if (isset($_GET['message']) and !wp_verify_nonce($_POST['message'],'name_of_my_action')) $temp['message'] = sanitize_text_field($_GET['message']);
-		if (isset($_GET['severadd']) and !wp_verify_nonce($_POST['serveradd'],'name_of_my_action')) $temp['serveradd'] = sanitize_text_field($_GET['serveradd']);
-		if (isset($_GET['smtpname']) and !wp_verify_nonce($_POST['smtpname'],'name_of_my_action')) $temp['smtpname'] = sanitize_text_field($_GET['smtpname']);
-		if (isset($_GET['smtpusername']) and !wp_verify_nonce($_POST['smtpusername'],'name_of_my_action') ) $temp['smtpusername'] = sanitize_text_field($_GET['smtpusername']);
-		if (isset($_GET['smtppass'])  and !wp_verify_nonce($_POST['smtppass'],'name_of_my_action')) $temp['smtppass'] = sanitize_text_field($_GET['smtppass']);
-		if (isset($_GET['smtppo'])  and !wp_verify_nonce($_POST['smtppo'],'name_of_my_action')) $temp['smtppo'] = sanitize_text_field($_GET['smtppo']);
-		if (isset($_GET['smtpsec'])  and !wp_verify_nonce($_POST['smtpsec'],'name_of_my_action')) $temp['smtpsec'] = sanitize_text_field($_GET['smtpsec']);
+		if (isset($_GET['email']) and !empty($_GET['email']) and !wp_verify_nonce($_POST['email'],'name_of_my_action')) $temp['email'] = ! wp_verify_nonce( sanitize_text_field( wp_unslash ( $_GET['email'] ) ) , 'name_of_my_action' );//sanitize_text_field($_GET['email']);
+		if (isset($_GET['subject']) and !wp_verify_nonce($_POST['subject'],'name_of_my_action')) $temp['subject'] = ! wp_verify_nonce( sanitize_text_field( wp_unslash ( $_GET['subject'] ) ) , 'name_of_my_action' ); //sanitize_text_field($_GET['subject']);
+		if (isset($_GET['message']) and !empty($_GET['severadd']) and !wp_verify_nonce($_POST['message'],'name_of_my_action')) $temp['message'] = ! wp_verify_nonce( sanitize_text_field( wp_unslash ( $_GET['message'] ) ) , 'name_of_my_action' ); //sanitize_text_field($_GET['message']);
+		if (isset($_GET['severadd']) and !empty($_GET['severadd']) and !wp_verify_nonce($_POST['serveradd'],'name_of_my_action')) $temp['serveradd'] = ! wp_verify_nonce( sanitize_text_field( wp_unslash ( $_GET['serveradd'] ) ) , 'name_of_my_action' );//sanitize_text_field($_GET['serveradd']);
+		if (isset($_GET['smtpname']) and !wp_verify_nonce($_POST['smtpname'],'name_of_my_action')) $temp['smtpname'] =! wp_verify_nonce( sanitize_text_field( wp_unslash ( $_GET['smtpname'] ) ) , 'name_of_my_action' ); //sanitize_text_field($_GET['smtpname']);
+		if (isset($_GET['smtpusername']) and !empty($_GET['smtpusername']) and !wp_verify_nonce($_POST['smtpusername'],'name_of_my_action') ) $temp['smtpusername'] =! wp_verify_nonce( sanitize_text_field( wp_unslash ( $_GET['smtpusername'] ) ) , 'name_of_my_action' ); //sanitize_text_field($_GET['smtpusername']);
+		if (isset($_GET['smtppass'])  and !wp_verify_nonce($_POST['smtppass'],'name_of_my_action')) $temp['smtppass'] = ! wp_verify_nonce( sanitize_text_field( wp_unslash ( $_GET['smtppass'] ) ) , 'name_of_my_action' );//sanitize_text_field($_GET['smtppass']);
+		if (isset($_GET['smtppo'])  and !wp_verify_nonce($_POST['smtppo'],'name_of_my_action')) $temp['smtppo'] = ! wp_verify_nonce( sanitize_text_field( wp_unslash ( $_GET['smtppo'] ) ) , 'name_of_my_action' );//sanitize_text_field($_GET['smtppo']);
+		if (isset($_GET['smtpsec'])  and !wp_verify_nonce($_POST['smtpsec'],'name_of_my_action')) $temp['smtpsec'] = ! wp_verify_nonce( sanitize_text_field( wp_unslash ( $_GET['smtpsec'] ) ) , 'name_of_my_action' );//sanitize_text_field($_GET['smtpsec']);
 		return array_filter($temp, function($x){
 			return $x;
 			});
